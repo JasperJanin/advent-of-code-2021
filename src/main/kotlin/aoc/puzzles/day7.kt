d79b2688e48f5478d15b80bf3451a3d1ed5aabea
jjanin <jasper.janin@ugent.be>
2021-12-07 23:31:14 +0100

    day 7

---
+++ src/main/kotlin/aoc/puzzles/day7.kt
@@ -0,0 +1,21 @@
+package aoc.puzzles
+
+import java.io.File
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+val crabsOneLine: (String, Int) -> Int =  { inputFile: String, part: Int -> File(inputFile).readLines()[0].split(",").map { it.toInt() }.let { input -> (input.minOrNull()!!..input.maxOrNull()!!).toList().map { pos -> input.sumOf { if (part == 1) abs(it - pos) else (abs(it - pos) * (abs(it - pos)+1)/2) } } }.minOrNull()!! }
+
+fun crabs(inputFile: String, part: Int): Int {
+    val input = File(inputFile).readLines()[0].split(",").map { it.toInt() }
+    var minFuel = 999999999
+    for (pos in input.minOrNull()!!..input.maxOrNull()!!) {
+        minFuel = min(input.sumOf { if (part == 1) abs(it - pos) else (abs(it - pos) * (abs(it - pos)+1)/2) }, minFuel)
+    }
+    return minFuel
+}
+
+fun main() {
+    println("Solution 1: ${crabs("inputs/day7", 1)}\nSolution 2: ${crabs("inputs/day7", 2)}")
+}
